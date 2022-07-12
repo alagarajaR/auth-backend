6aa0bdeb8c13b60d591acd9fde287a059c32801f
alagarajaR <alagarajaR@gmail.com>
2022-07-12 16:54:54 +0530

    Auth Both and Frontend and backend

---
+++ react-auth/src/Register.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(false);
+
+  // set configurations
+
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    const configuration = {
+      method: "post",
+      url: "http://localhost:3001/register",
+      data: {
+        email,
+        password,
+      },
+    };
+
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        setRegister(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+
+    // make a popup alert showing the "submitted" text
+    alert("Submited");
+  };
+
+  return (
+    <>
+      <h2>Register</h2>
+      <Form>
+        {/* display success message */}
+        {register ? (
+          <p className="text-success">You Are Registered Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Registered</p>
+        )}
+        {/* email */}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        {/* submit button */}
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+}
